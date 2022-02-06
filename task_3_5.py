5a6aa5ffea307e2cfd4d06ff0d9a5eea1b4c84a1
fxshad <fxshad@mail.ru>
2022-02-06 15:12:43 +0700

    +doc 3_5

--- task_3_5.py
+++ task_3_5.py
@@ -28,31 +28,32 @@ print(get_jokes(2))
 print(get_jokes(10))
 
 
-print("\n")                                                      ##### документирование, флаг и именованные аргументы
+print("\n")
 def get_jokes_adv(count) -> list:
+    """Return list count of jokes No repeat words """
      # пишите реализацию здесь
-     list_out = []
+    list_out = []
+    if count > min(len(nouns), len(adverbs), len(adjectives)):
+        count = min(len(nouns), len(adverbs), len(adjectives))
+    if count < 1:
+        count = 0
+    idx = 1
+    list_out.extend(random.sample(nouns, k=count, counts=None))
+    for adv in random.sample(adverbs, k=count, counts=None):
+        list_out.insert(idx, adv)
+        idx += 2
+    idx = 2
+    for adj in random.sample(adjectives, k=count, counts=None):
+        list_out.insert(idx, adj)
+        idx += 3
+        _ = 0
+    for i in range(0, len(list_out)):
+        new_str = ''+' '.join(list(map(str, list_out[_:_+3])))
+        _ += 4
+        list_out.insert (i, new_str)
+    list_out = list_out[0:count]
+    return list_out
 
-     if count > min(len(nouns), len(adverbs), len(adjectives)):
-         count = min(len(nouns), len(adverbs), len(adjectives))
-     if count < 1:
-         count = 0
-     idx = 1
-     list_out.extend(random.sample(nouns, k=count, counts=None))
-     for adv in random.sample(adverbs, k=count, counts=None):
-         list_out.insert(idx, adv)
-         idx += 2
-     idx = 2
-     for adj in random.sample(adjectives, k=count, counts=None):
-         list_out.insert(idx, adj)
-         idx += 3
-         _ = 0
-     for i in range(0, len(list_out)):
-         new_str = ''+' '.join(list(map(str, list_out[_:_+3])))
-         _ += 4
-         list_out.insert (i, new_str)
-     list_out = list_out[0:count]
-     return list_out
 
 print(get_jokes_adv(66))
 
