73851a1a60974ed20d888be533edcc784bd99d0d
fxshad <fxshad@mail.ru>
2022-02-10 20:31:39 +0700

    0

---
+++ task_4_4.py
@@ -0,0 +1,15 @@
+from utils import currency_rates
+
+rez1 = currency_rates("AUD")
+rez2 = currency_rates("kzt")
+rez3 = currency_rates("CNY")
+rez4 = currency_rates("uah")
+
+
+print(f'{rez1}\n{rez2}\n{rez3}\n{rez4}')
+
+
+
+"""
+
+"""
